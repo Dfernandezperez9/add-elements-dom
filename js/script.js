39e2a59001c4990e808d54108ebb41ad7ee9b036
David <dfernandezperez9@gmail.com>
2024-06-09 00:17:22 +0200

    Completado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+
+ const boton = document.getElementById("agregar");
+ const lista = document.getElementById("lista");
+ 
+
+ 
+
+ boton.addEventListener("click", function () {
+    let input = prompt("introduce un nuevo elemento: ");
+    let newElement = document.createElement("li");
+    lista.append(newElement);
+    newElement.innerHTML = input;
+    
+    
+})
